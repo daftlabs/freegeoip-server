fd5b75e328f53b126f5eba544db98daca80ed170
Max Eddy <max@daftlabs.com>
2014-12-18 13:25:46 -0500

    added ssh/git stuff for chef server compatibility

--- cookbooks/freegeoip-server/metadata.rb
+++ cookbooks/freegeoip-server/metadata.rb
@@ -1,5 +1,6 @@
 name             'freegeoip-server'
 maintainer       'daft labs'
+maintainer_email 'support@daftlabs.com'
 license          'MIT'
 description      'serves freegeoip'
 version          '0.1.0'
--- cookbooks/freegeoip-server/recipes/default.rb
+++ cookbooks/freegeoip-server/recipes/default.rb
@@ -1,5 +1,34 @@
 require 'fileutils'
 
+unless Chef::Config[:solo] then
+  directory "/var/www/.ssh" do
+    recursive true
+  end
+
+  rsa_key = data_bag_item('deploy_keys', 'freegeoip')['key']
+
+  file "/var/www/.ssh/id_rsa" do
+    content rsa_key
+    mode 00600
+  end
+
+  ssh_wrapper_file = "/var/www/.ssh/.gitssh"
+
+  file ssh_wrapper_file do
+    content "exec ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -i \"/var/www/.ssh/id_rsa\" \"$@\""
+    mode 00755
+  end
+
+  git repo_directory do
+    repository "git@github.com:daftlabs/freegeoip-server.git"
+    enable_submodules true
+    ssh_wrapper ssh_wrapper_file
+    if node.attribute?(:git_ref) then
+      revision node.git_ref
+    end
+  end
+end
+
 ruby_block "install freegeoip" do
   block do
     FileUtils.cp(node['freegeoip_path'] + '/freegeoip', '/usr/bin/freegeoip')
