4f527f1f137624b0e4871d8ff525d665d3718cac
Max Eddy <max@daftlabs.com>
2014-12-18 11:02:50 -0500

    node attribute fixes

---
+++ cookbooks/freegeoip-server/attributes/default.rb
@@ -0,0 +1,4 @@
+default['freegeoip_path'] = '/var/www/freegeoip-server/vendor/freegeoip-3.0.2-linux-amd64'
+default['service_name'] = 'freegeoip'
+default['bin_path'] = '/usr/bin/freegeoip'
+default['service_path'] = '/etc/init.d/freegeoip'
--- cookbooks/freegeoip-server/recipes/default.rb
+++ cookbooks/freegeoip-server/recipes/default.rb
@@ -2,20 +2,17 @@ require 'fileutils'
 
 ruby_block "install freegeoip" do
   block do
-    FileUtils.cp(node['freegeoip-pathr'] + '/freegeoip', '/user/bin/')
+    FileUtils.cp(node['freegeoip_path'] + '/freegeoip', '/usr/bin/freegeoip')
   end
-  not_if { File.exist?('/user/bin/freegeoip') }
+  not_if { File.exist?('/usr/bin/freegeoip') }
 end
 
 template node['service_path'] do
   source "service.erb"
+  mode "0755"
 end
 
-service node['service_name'] do
+service 'freegeoip' do
   action :start
 end
 
-execute "start freegeoip service" do
-  cwd "/var/www/freegeoip-server/vendor/freegeoip-3.0.2-linux-amd64"
-  command "./freegeoip"
-end
